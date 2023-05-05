c0359fd64d2dcbdcf41d32cd9e9d354c4e42ec93
evan <evanbonner99@gmail.com>
2023-05-05 05:41:13 +0100

    Implment basic builtin functions
    Implements @sum, @prod, @avg, @max & @min functions. All take a variadic number of expressions
    separated by commas.

--- src/Parse.zig
+++ src/Parse.zig
@@ -120,7 +120,14 @@ pub fn splice(ast: *Ast, new_root: u32) void {
 				b.lhs -= context.first_node;
 				b.rhs -= context.first_node;
 			},
-			else => {},
+			.builtin => |*b| {
+				b.args -= context.first_node;
+			},
+			.arg_list => |*b| {
+				b.start -= context.first_node;
+				b.end -= context.first_node;
+			},
+			.number, .column, .cell => {},
 		}
 		slice.set(i, n);
 	}
@@ -129,29 +136,64 @@ pub fn splice(ast: *Ast, new_root: u32) void {
 }
 
 pub fn print(ast: *Ast, writer: anytype) @TypeOf(writer).Error!void {
-	const Context = struct {
-		ast: *const Ast,
-		writer: *const @TypeOf(writer),
+	return ast.printFrom(ast.rootNodeIndex(), writer);
+}
 
-		pub fn evalCell(context: @This(), index: u32) !bool {
-			const node = context.ast.nodes.get(index);
+pub fn printFrom(ast: *Ast, index: u32, writer: anytype) !void {
+	const node = ast.nodes.get(index);
 
-			switch (node) {
-				.number => |n| try context.writer.print("{d}", .{ n }),
-				.column => |col| try Position.writeColumnAddress(col, context.writer.*),
-				.cell => |pos| try pos.writeCellAddress(context.writer.*),
-				.assignment => try context.writer.writeAll(" = "),
-				.add => try context.writer.writeAll(" + "),
-				.sub => try context.writer.writeAll(" - "),
-				.mul => try context.writer.writeAll(" * "),
-				.div => try context.writer.writeAll(" / "),
-				.mod => try context.writer.writeAll(" % "),
+	switch (node) {
+		.number => |n| try writer.print("{d}", .{ n }),
+		.column => |col| try Position.writeColumnAddress(col, writer),
+		.cell => |pos| try pos.writeCellAddress(writer),
+
+		.assignment => |b| {
+			try ast.printFrom(b.lhs, writer);
+			try writer.writeAll(" = ");
+			try ast.printFrom(b.rhs, writer);
+		},
+		.add => |b| {
+			try ast.printFrom(b.lhs, writer);
+			try writer.writeAll(" + ");
+			try ast.printFrom(b.rhs, writer);
+		},
+		.sub => |b| {
+			try ast.printFrom(b.lhs, writer);
+			try writer.writeAll(" - ");
+			try ast.printFrom(b.rhs, writer);
+		},
+		.mul => |b| {
+			try ast.printFrom(b.lhs, writer);
+			try writer.writeAll(" * ");
+			try ast.printFrom(b.rhs, writer);
+		},
+		.div => |b| {
+			try ast.printFrom(b.lhs, writer);
+			try writer.writeAll(" / ");
+			try ast.printFrom(b.rhs, writer);
+		},
+		.mod => |b| {
+			try ast.printFrom(b.lhs, writer);
+			try writer.writeAll(" % ");
+			try ast.printFrom(b.rhs, writer);
+		},
+
+		.builtin => |b| {
+			switch (b.tag) {
+				inline else => |tag| try writer.print("@{s}(", .{ @tagName(tag) }),
 			}
-			return true;
-		}
-	};
+			try ast.printFrom(b.args, writer);
+			try writer.writeByte(')');
+		},
 
-	_ = try ast.traverse(.middle, Context{ .ast = ast, .writer = &writer });
+		.arg_list => |args| {
+			try ast.printFrom(args.start, writer);
+			for (args.start + 1..args.end) |i| {
+				try writer.writeAll(", ");
+				try ast.printFrom(@intCast(u32, i), writer);
+			}
+		},
+	}
 }
 
 /// The order in which nodes are evaluated.
@@ -200,6 +242,23 @@ fn traverseFrom(ast: Ast, index: u32, order: TraverseOrder, context: anytype) !b
 		.number, .cell, .column => {
 			if (!try context.evalCell(index)) return false;
 		},
+		.builtin => |b| {
+			if (!try ast.traverseFrom(b.args, order, context)) return false;
+		},
+		.arg_list => |b| switch (order) {
+			.first, .middle => {
+				if (!try context.evalCell(index)) return false;
+				for (b.start..b.end) |i| {
+					if (!try ast.traverseFrom(@intCast(u32, i), order, context)) return false;
+				}
+			},
+			.last => {
+				for (b.start..b.end) |i| {
+					if (!try ast.traverseFrom(@intCast(u32, i), order, context)) return false;
+				}
+				if (!try context.evalCell(index)) return false;
+			},
+		},
 	}
 
 	return true;
@@ -226,7 +285,51 @@ pub fn evalNode(ast: Ast, index: u32, total: f64, context: anytype) f64 {
 		.mul => |op| total + (ast.evalNode(op.lhs, total, context) * ast.evalNode(op.rhs, total, context)),
 		.div => |op| total + (ast.evalNode(op.lhs, total, context) / ast.evalNode(op.rhs, total, context)),
 		.mod => |op| total + @rem(ast.evalNode(op.lhs, total, context), ast.evalNode(op.rhs, total, context)),
-		else => unreachable,
+		.builtin => |b| {
+			const args = ast.nodes.items(.data)[b.args].arg_list;
+			switch (b.tag) {
+				.sum => {
+					var sum_total: f64 = 0;
+					for (args.start..args.end) |i| {
+						sum_total += ast.evalNode(@intCast(u32, i), sum_total, context);
+					}
+					return total + sum_total;
+				},
+				.prod => {
+					var prod_total: f64 = 1;
+					for (args.start..args.end) |i| {
+						prod_total *= ast.evalNode(@intCast(u32, i), prod_total, context);
+					}
+					return total + prod_total;
+				},
+				.avg => {
+					var sum_total: f64 = 0;
+					for (args.start..args.end) |i| {
+						sum_total = ast.evalNode(@intCast(u32, i), sum_total, context);
+					}
+					return total + sum_total / @intToFloat(f64, (args.end - args.start));
+				},
+				.max => {
+					var max = ast.evalNode(args.start, 0, context);
+					for (args.start + 1..args.end) |i| {
+						const val = ast.evalNode(@intCast(u32, i), 0, context);
+						if (val > max)
+							max = val;
+					}
+					return total + max;
+				},
+				.min => {
+					var min = ast.evalNode(args.start, 0, context);
+					for (args.start + 1..args.end) |i| {
+						const val = ast.evalNode(@intCast(u32, i), 0, context);
+						if (val < min)
+							min = val;
+					}
+					return total + min;
+				},
+			}
+		},
+		.column, .assignment, .arg_list => unreachable,
 	};
 }
 
@@ -240,12 +343,48 @@ pub const Node = union(enum) {
 	mul: BinaryOperator,
 	div: BinaryOperator,
 	mod: BinaryOperator,
+	builtin: Builtin,
+	arg_list: ArgList,
+};
+
+comptime {
+	assert(@sizeOf(Node) == 16);
+}
+
+const Builtin = struct {
+	tag: Tag,
+	args: u32,
+
+	const Tag = enum {
+		sum,
+		prod,
+		avg,
+		max,
+		min,
+	};
+
+	comptime {
+		assert(@sizeOf(Tag) <= 4);
+	}
+};
+
+const ArgList = packed struct(u64) {
+	start: u32,
+	end: u32,
 };
 
 const BinaryOperator = struct {
 	lhs: u32,
 	rhs: u32,
 };
+
+const builtins = std.ComptimeStringMap(Builtin.Tag, .{
+	.{ "sum", .sum },
+	.{ "prod", .prod },
+	.{ "avg", .avg },
+	.{ "max", .max },
+	.{ "min", .min },
+});
 
 const Parser = struct {
 	source: []const u8,
@@ -262,23 +401,23 @@ const Parser = struct {
 
 	const log = std.log.scoped(.parser);
 
-	fn parse(ast: *Parser) !void {
-		_ = try ast.parseStatement();
-		_ = try ast.expectToken(.eof);
+	fn parse(parser: *Parser) !void {
+		_ = try parser.parseStatement();
+		_ = try parser.expectToken(.eof);
 	}
 
 	/// Statement <- Assignment
-	fn parseStatement(ast: *Parser) !u32 {
-		return ast.parseAssignment();
+	fn parseStatement(parser: *Parser) !u32 {
+		return parser.parseAssignment();
 	}
 
 	/// Assignment <- CellName '=' Expression
-	fn parseAssignment(ast: *Parser) !u32 {
-		const lhs = try ast.parseCellName();
-		_ = try ast.expectToken(.equals_sign);
-		const rhs = try ast.parseExpression();
+	fn parseAssignment(parser: *Parser) !u32 {
+		const lhs = try parser.parseCellName();
+		_ = try parser.expectToken(.equals_sign);
+		const rhs = try parser.parseExpression();
 
-		return ast.addNode(.{
+		return parser.addNode(.{
 			.assignment = .{
 				.lhs = lhs,
 				.rhs = rhs,
@@ -289,132 +428,176 @@ const Parser = struct {
 	const ParseError = error{ UnexpectedToken } || Allocator.Error;
 
 	/// Expression <- AddExpr
-	fn parseExpression(ast: *Parser) ParseError!u32 {
-		return ast.parseAddExpr();
+	fn parseExpression(parser: *Parser) ParseError!u32 {
+		return parser.parseAddExpr();
 	}
 
 	/// AddExpr <- MulExpr (('+' / '-') MulExpr)*
-	fn parseAddExpr(ast: *Parser) !u32 {
-		var index = try ast.parseMulExpr();
+	fn parseAddExpr(parser: *Parser) !u32 {
+		var index = try parser.parseMulExpr();
 
-		while (ast.eatTokenMulti(.{ .plus, .minus })) |i| {
-			log.info("INDEX: {d}", .{ index });
+		while (parser.eatTokenMulti(.{ .plus, .minus })) |i| {
 			const op = BinaryOperator{
 				.lhs = index,
-				.rhs = try ast.parseMulExpr(),
+				.rhs = try parser.parseMulExpr(),
 			};
 
-			const node: Node = switch (ast.token_tags[i]) {
+			const node: Node = switch (parser.token_tags[i]) {
 				.plus => .{ .add = op },
 				.minus => .{ .sub = op },
 				else => unreachable,
 			};
 
-			index = try ast.addNode(node);
+			index = try parser.addNode(node);
 		}
 
-		log.info("RETURNING {d}", .{ index });
 		return index;
 	}
 
 	/// MulExpr <- PrimaryExpr (('*' / '/' / '%') PrimaryExpr)*
-	fn parseMulExpr(ast: *Parser) !u32 {
-		var index = try ast.parsePrimaryExpr();
+	fn parseMulExpr(parser: *Parser) !u32 {
+		var index = try parser.parsePrimaryExpr();
 
-		while (ast.eatTokenMulti(.{ .asterisk, .forward_slash, .percent })) |i| {
+		while (parser.eatTokenMulti(.{ .asterisk, .forward_slash, .percent })) |i| {
 			const op = BinaryOperator{
 				.lhs = index,
-				.rhs = try ast.parsePrimaryExpr(),
+				.rhs = try parser.parsePrimaryExpr(),
 			};
 
-			const node: Node = switch (ast.token_tags[i]) {
+			const node: Node = switch (parser.token_tags[i]) {
 				.asterisk => .{ .mul = op },
 				.forward_slash => .{ .div = op },
 				.percent => .{ .mod = op },
 				else => unreachable,
 			};
 
-			index = try ast.addNode(node);
+			index = try parser.addNode(node);
 		}
 
 		return index;
 	}
 
-	/// PrimaryExpr <- Number / CellName / '(' Expression ')'
-	fn parsePrimaryExpr(ast: *Parser) !u32 {
-		return switch (ast.token_tags[ast.tok_i]) {
-			.number => ast.parseNumber(),
-			.cell_name => ast.parseCellName(),
+	/// PrimaryExpr <- Number / CellName / Builtin / '(' Expression ')'
+	fn parsePrimaryExpr(parser: *Parser) !u32 {
+		return switch (parser.token_tags[parser.tok_i]) {
+			.minus, .plus, .number => parser.parseNumber(),
+			.cell_name => parser.parseCellName(),
 			.lparen => {
-				_ = try ast.expectToken(.lparen);
-				const ret = ast.parseExpression();
-				_ = try ast.expectToken(.rparen);
+				_ = try parser.expectToken(.lparen);
+				const ret = parser.parseExpression();
+				_ = try parser.expectToken(.rparen);
 				return ret;
 			},
+			.builtin => parser.parseFunction(),
 			else => error.UnexpectedToken,
 		};
 	}
 
-	fn parseNumber(ast: *Parser) !u32 {
-		const i = try ast.expectToken(.number);
-		const text = ast.tokenContent(i).text(ast.source);
+	/// Builtin <- builtin '(' ArgList? ')'
+	fn parseFunction(parser: *Parser) !u32 {
+		const token_index = try parser.expectToken(.builtin);
+		_ = try parser.expectToken(.lparen);
+
+		const identifier = parser.tokenContent(token_index).text(parser.source);
+		const builtin = builtins.get(identifier) orelse return error.UnexpectedToken;
+
+		const args = try parser.parseArgList();
+		_ = try parser.expectToken(.rparen);
+		return parser.addNode(.{
+			.builtin = .{
+				.tag = builtin,
+				.args = args,
+			},
+		});
+	}
+
+	/// ArgList <- Expression (',' Expression)*
+	fn parseArgList(parser: *Parser) !u32 {
+		const start = try parser.parseExpression();
+		var end = start + 1;
+
+		while (parser.eatToken(.comma)) |_| {
+			end = 1 + try parser.parseExpression();
+		}
+
+		return parser.addNode(.{
+			.arg_list = .{
+				.start = start,
+				.end = end,
+			},
+		});
+	}
+
+	/// Number <- ('+' / '-')? ('0'-'9')+
+	fn parseNumber(parser: *Parser) !u32 {
+		const is_positive = parser.eatToken(.minus) == null;
+		if (is_positive) _ = parser.eatToken(.plus);
+
+		const i = try parser.expectToken(.number);
+		const text = parser.tokenContent(i).text(parser.source);
 
 		// Correctness of the number is guaranteed because the tokenizer wouldn't have generated a
 		// number token on invalid format.
 		const num = std.fmt.parseFloat(f64, text) catch unreachable;
 
-		return ast.addNode(.{
-			.number = num,
+		return parser.addNode(.{
+			.number = if (is_positive) num else -num,
 		});
 	}
 
 	/// CellName <- ('a'-'z' / 'A'-'Z')+ ('0'-'9')+
-	fn parseCellName(ast: *Parser) !u32 {
-		const i = try ast.expectToken(.cell_name);
-		const text = ast.tokenContent(i).text(ast.source);
+	fn parseCellName(parser: *Parser) !u32 {
+		const i = try parser.expectToken(.cell_name);
+		const text = parser.tokenContent(i).text(parser.source);
 
 		// TODO: check bounds
 		const pos = Position.fromCellAddress(text);
 
-		return ast.addNode(.{
+		return parser.addNode(.{
 			.cell = pos,
 		});
 	}
 
-	fn tokenContent(ast: Parser, index: u32) Token {
+	fn tokenContent(parser: Parser, index: u32) Token {
 		return .{
-			.tag = ast.token_tags[index],
-			.start = ast.token_starts[index],
-			.end = ast.token_ends[index],
+			.tag = parser.token_tags[index],
+			.start = parser.token_starts[index],
+			.end = parser.token_ends[index],
 		};
 	}
 
-	fn addNode(ast: *Parser, data: Node) Allocator.Error!u32 {
-		const ret = @intCast(u32, ast.nodes.len);
-		try ast.nodes.append(ast.allocator, data);
+	fn addNode(parser: *Parser, data: Node) Allocator.Error!u32 {
+		const ret = @intCast(u32, parser.nodes.len);
+		try parser.nodes.append(parser.allocator, data);
 		return ret;
 	}
 
-	fn expectToken(ast: *Parser, expected_tag: Token.Tag) !u32 {
-		return ast.eatToken(expected_tag) orelse error.UnexpectedToken;
+	fn expectToken(parser: *Parser, expected_tag: Token.Tag) !u32 {
+		return parser.eatToken(expected_tag) orelse {
+			log.warn("Unexpected token {s}, expected {s}", .{
+				@tagName(parser.token_tags[parser.tok_i]),
+				@tagName(expected_tag),
+			});
+			return error.UnexpectedToken;
+		};
 	}
 
-	fn eatToken(ast: *Parser, expected_tag: Token.Tag) ?u32 {
-		return if (ast.token_tags[ast.tok_i] == expected_tag) ast.nextToken() else null;
+	fn eatToken(parser: *Parser, expected_tag: Token.Tag) ?u32 {
+		return if (parser.token_tags[parser.tok_i] == expected_tag) parser.nextToken() else null;
 	}
 
-	fn eatTokenMulti(ast: *Parser, tags: anytype) ?u32 {
+	fn eatTokenMulti(parser: *Parser, tags: anytype) ?u32 {
 		inline for (tags) |tag| {
-			if (ast.eatToken(tag)) |token|
+			if (parser.eatToken(tag)) |token|
 				return token;
 		}
 
 		return null;
 	}
 
-	fn nextToken(ast: *Parser) u32 {
-		const ret = ast.tok_i;
-		ast.tok_i += 1;
+	fn nextToken(parser: *Parser) u32 {
+		const ret = parser.tok_i;
+		parser.tok_i += 1;
 		return ret;
 	}
 };
@@ -437,12 +620,14 @@ pub const Token = struct {
 		asterisk,
 		forward_slash,
 		percent,
+		comma,
 
 		lparen,
 		rparen,
 
 		column_name,
 		cell_name,
+		builtin,
 
 		eof,
 		invalid,
@@ -456,6 +641,7 @@ pub const Tokenizer = struct {
 	const State = enum {
 		start,
 		number,
+		builtin,
 		column_or_cell_name,
 	};
 
@@ -491,6 +677,16 @@ pub const Tokenizer = struct {
 					'a'...'z', 'A'...'Z' => {
 						state = .column_or_cell_name;
 						tag = .column_name;
+					},
+					'@' => {
+						state = .builtin;
+						tag = .builtin;
+						start += 1;
+					},
+					',' => {
+						tag = .comma;
+						tokenizer.pos += 1;
+						break;
 					},
 					'+' => {
 						tag = .plus;
@@ -545,6 +741,10 @@ pub const Tokenizer = struct {
 					'0'...'9' => {
 						tag = .cell_name;
 					},
+					else => break,
+				},
+				.builtin => switch (c) {
+					'a'...'z', 'A'...'Z', '_' => {},
 					else => break,
 				},
 			}
