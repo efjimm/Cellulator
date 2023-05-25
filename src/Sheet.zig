efjimm/Cellulator

commit 5ecf8952986d89c373c0fcfd649cc6bf2d598b6a

MAJOR performance bug fix for cell evaluation
Previoulsy, cell evaluations were unknowingly not being cached. This meant that every cell had to
evaluate all of its dependents recursively, even if they had already been evaluated. This commit
fixes that and makes some other small changes. Update time for `example.zc` went from 14.1 seconds
to 0.003 seconds in a debug build.

This would have been caught sooner if I wrote more extensive tests 🙃

example.zc
@@ -1,0 +1,148 @@
+A0 = 1
+B0 = A0+1
+C0 = B0+1
+D0 = C0+1
+E0 = D0+1
+F0 = E0+1
+G0 = F0+1
+H0 = G0+1
+A1 = A0+1
+B1 = A1+B0
+C1 = C0+B1
+D1 = D0+C1
+E1 = E0+D1
+F1 = F0+E1
+G1 = G0+F1
+A2 = A1+1
+B2 = A2+B1
+C2 = C1+B2
+D2 = D1+C2
+E2 = E1+D2
+F2 = F1+E2
+G2 = G1+F2
+A3 = A2+1
+B3 = A3+B2
+C3 = C2+B3
+D3 = D2+C3
+E3 = E2+D3
+F3 = F2+E3
+G3 = G2+F3
+A4 = A3+1
+B4 = A4+B3
+C4 = C3+B4
+D4 = D3+C4
+E4 = E3+D4
+F4 = F3+E4
+G4 = G3+F4
+A5 = A4+1
+B5 = A5+B4
+C5 = C4+B5
+D5 = D4+C5
+E5 = E4+D5
+F5 = F4+E5
+G5 = G4+F5
+A6 = A5+1
+B6 = A6+B5
+C6 = C5+B6
+D6 = D5+C6
+E6 = E5+D6
+F6 = F5+E6
+G6 = G5+F6
+A7 = A6+1
+B7 = A7+B6
+C7 = C6+B7
+D7 = D6+C7
+E7 = E6+D7
+F7 = F6+E7
+G7 = G6+F7
+A8 = A7+1
+B8 = A8+B7
+C8 = C7+B8
+D8 = D7+C8
+E8 = E7+D8
+F8 = F7+E8
+G8 = G7+F8
+A9 = A8+1
+B9 = A9+B8
+C9 = C8+B9
+D9 = D8+C9
+E9 = E8+D9
+F9 = F8+E9
+G9 = G8+F9
+A10 = A9+1
+B10 = A10+B9
+C10 = C9+B10
+D10 = D9+C10
+E10 = E9+D10
+F10 = F9+E10
+G10 = G9+F10
+A11 = A10+1
+B11 = A11+B10
+C11 = C10+B11
+D11 = D10+C11
+E11 = E10+D11
+F11 = F10+E11
+G11 = G10+F11
+A12 = A11+1
+B12 = A12+B11
+C12 = C11+B12
+D12 = D11+C12
+E12 = E11+D12
+F12 = F11+E12
+G12 = G11+F12
+A13 = A12+1
+B13 = A13+B12
+C13 = C12+B13
+D13 = D12+C13
+E13 = E12+D13
+F13 = F12+E13
+G13 = G12+F13
+A14 = A13+1
+B14 = A14+B13
+C14 = C13+B14
+D14 = D13+C14
+E14 = E13+D14
+F14 = F13+E14
+G14 = G13+F14
+A15 = A14+1
+B15 = A15+B14
+C15 = C14+B15
+D15 = D14+C15
+E15 = E14+D15
+F15 = F14+E15
+G15 = G14+F15
+A16 = A15+1
+B16 = A16+B15
+C16 = C15+B16
+D16 = D15+C16
+E16 = E15+D16
+F16 = F15+E16
+G16 = G15+F16
+A17 = A16+1
+B17 = A17+B16
+C17 = C16+B17
+D17 = D16+C17
+E17 = E16+D17
+F17 = F16+E17
+G17 = G16+F17
+A18 = A17+1
+B18 = A18+B17
+C18 = C17+B18
+D18 = D17+C18
+E18 = E17+D18
+F18 = F17+E18
+G18 = G17+F18
+A19 = A18+1
+B19 = A19+B18
+C19 = C18+B19
+D19 = D18+C19
+E19 = E18+D19
+F19 = F18+E19
+G19 = G18+F19
+A20 = A19+1
+B20 = A20+B19
+C20 = C19+B20
+D20 = D19+C20
+E20 = E19+D20
+F20 = F19+E20
+G20 = G19+F20

src/Sheet.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const builtin = @import("builtin");
 const utils = @import("utils.zig");
 const Ast = @import("Parse.zig");
 
@@ -11,7 +12,7 @@ const NodeListUnmanaged = std.ArrayListUnmanaged(Position);
 
 const log = std.log.scoped(.sheet);
 
-const PositionContext = struct {
+const ArrayPositionContext = struct {
 	pub fn eql(_: @This(), p1: Position, p2: Position, _: usize) bool {
 		return p1.y == p2.y and p1.x == p2.x;
 	}
@@ -21,7 +22,27 @@ const PositionContext = struct {
 	}
 };
 
-const CellMap = std.ArrayHashMapUnmanaged(Position, Cell, PositionContext, false);
+/// std.HashMap and std.ArrayHasMap require slightly different contexts.
+const PositionContext = struct {
+	pub fn eql(_: @This(), p1: Position, p2: Position) bool {
+		return p1.y == p2.y and p1.x == p2.x;
+	}
+
+	pub fn hash(_: @This(), pos: Position) u64 {
+		return pos.hash();
+	}
+};
+
+const CellMap = std.ArrayHashMapUnmanaged(Position, Cell, ArrayPositionContext, false);
+
+/// Used for cell evaluation
+const NodeMap = std.HashMapUnmanaged(Position, NodeMark, PositionContext, 99);
+
+/// Used for cell evaluation
+const NodeMark = enum {
+	temporary,
+	permanent,
+};
 
 /// ArrayHashMap mapping spreadsheet positions to cells.
 cells: CellMap = .{},
@@ -30,9 +51,15 @@ cells: CellMap = .{},
 columns: std.AutoArrayHashMapUnmanaged(u16, Column) = .{},
 filepath: std.BoundedArray(u8, std.fs.MAX_PATH_BYTES) = .{},
 
+/// Map containing cells which have already been visited during evaluation.node_list
+visited_nodes: NodeMap = .{},
 /// Cell positions sorted topologically, used for order of evaluation when evaluating all cells.
 sorted_nodes: NodeListUnmanaged = .{},
+
+/// If true, the next call to Sheet.update will re-evaluate all cells in the sheet.
 needs_update: bool = false,
+
+/// True if there have been any changes since the last save
 has_changes: bool = false,
 
 allocator: Allocator,
@@ -49,6 +76,7 @@ pub fn deinit(sheet: *Sheet) void {
 	}
 
 	sheet.cells.deinit(sheet.allocator);
+	sheet.visited_nodes.deinit(sheet.allocator);
 	sheet.sorted_nodes.deinit(sheet.allocator);
 	sheet.columns.deinit(sheet.allocator);
 	sheet.* = undefined;
@@ -144,68 +172,64 @@ pub fn getCellPtr(sheet: *Sheet, pos: Position) ?*Cell {
 	return sheet.cells.getPtr(pos);
 }
 
-const NodeMark = enum {
-	temporary,
-	permanent,
-};
-
-/// Re-evaluates all cells in the sheet. It evaluates cells in reverse topological order to ensure
-/// that we only need to evaluate each cell once. Cell results are cached after they are evaluted
+/// Re-evaluates all cells in the sheet. Evaluates cells in reverse topological order to ensure
+/// that each cell is only evaluated once. Cell results are cached after they are evaluted
 /// (see Cell.eval)
 pub fn update(sheet: *Sheet) Allocator.Error!void {
-	if (!sheet.needs_update)
-		return;
+	if (!sheet.needs_update) return;
+
+	log.debug("Updating...", .{});
+
+	// Is void unless in debug build
+	const begin = (if (builtin.mode == .Debug) std.time.Instant.now() catch unreachable else {});
 
 	try sheet.rebuildSortedNodeList();
 
-	var iter = std.mem.reverseIterator(sheet.sorted_nodes.items);
-	while (iter.next()) |pos| {
+	// Mark all cells as needing to be re-evaluated
+	for (sheet.cells.values()) |*cell| {
+		cell.num = null;
+	}
+
+	for (sheet.sorted_nodes.items) |pos| {
 		const cell = sheet.getCellPtr(pos).?;
-		_ = cell.eval(sheet);
+		_ = try cell.eval(sheet);
 	}
 
 	sheet.needs_update = false;
+	if (builtin.mode == .Debug) {
+		log.debug("Finished update in {d} seconds", .{
+			blk: {
+				const now = std.time.Instant.now() catch unreachable;
+				const elapsed = now.since(begin);
+				break :blk @intToFloat(f64, elapsed) / std.time.ns_per_s;
+			},
+		});
+	}
 }
-
-const NodeMap = std.HashMap(Position, NodeMark, struct {
-	pub fn eql(_: @This(), p1: Position, p2: Position) bool {
-		return p1.y == p2.y and p1.x == p2.x;
-	}
-
-	pub fn hash(_: @This(), pos: Position) u64 {
-		return pos.hash();
-	}
-}, 99);
 
 fn rebuildSortedNodeList(sheet: *Sheet) Allocator.Error!void {
 	const node_count = @intCast(u32, sheet.cells.entries.len);
 
 	// Topologically sorted set of cell positions
-	var nodes = sheet.sorted_nodes.toManaged(sheet.allocator);
-	defer sheet.sorted_nodes = nodes.moveToUnmanaged();
-	nodes.clearRetainingCapacity();
-
-	var visited_nodes = NodeMap.init(sheet.allocator);
-	defer visited_nodes.deinit();
-
-	try nodes.ensureTotalCapacity(node_count + 1);
-	try visited_nodes.ensureTotalCapacity(node_count + 1);
+	sheet.visited_nodes.clearRetainingCapacity();
+	sheet.sorted_nodes.clearRetainingCapacity();
+	try sheet.visited_nodes.ensureTotalCapacity(sheet.allocator, node_count + 1);
+	try sheet.sorted_nodes.ensureTotalCapacity(sheet.allocator, node_count + 1);
 
 	for (sheet.cells.keys()) |pos| {
-		if (!visited_nodes.contains(pos))
-			try visit(sheet, pos, &nodes, &visited_nodes);
+		if (!sheet.visited_nodes.contains(pos))
+			visit(sheet, pos, &sheet.visited_nodes, &sheet.sorted_nodes);
 	}
 
 }
 
-/// Recursive function that visits every dependency of a cell.
 fn visit(
 	sheet: *const Sheet,
 	node: Position,
-	nodes: *NodeList,
-	visited_nodes: *NodeMap,
-) Allocator.Error!void {
-	if (visited_nodes.get(node)) |mark| {
+	nodes: *NodeMap,
+	sorted_nodes: *NodeListUnmanaged,
+) void {
+	if (nodes.get(node)) |mark| {
 		switch (mark) {
 			.permanent => return,
 			.temporary => unreachable,
@@ -214,23 +238,18 @@ fn visit(
 
 	var cell = sheet.getCell(node) orelse return;
 
-	visited_nodes.putAssumeCapacity(node, .temporary);
+	nodes.putAssumeCapacity(node, .temporary);
 
 	const Context = struct {
 		sheet: *const Sheet,
-		node: Position,
-		nodes: *NodeList,
-		visited_nodes: *NodeMap,
+		cell: *Cell,
+		nodes: *NodeMap,
+		sorted_nodes: *NodeListUnmanaged,
 
-		pub fn evalCell(context: @This(), index: u32) Allocator.Error!bool {
-			const _cell = context.sheet.getCell(context.node).?;
-			const ast_node = _cell.ast.nodes.get(index);
+		pub fn evalCell(context: @This(), index: u32) !bool {
+			const ast_node = context.cell.ast.nodes.get(index);
 			if (ast_node == .cell) {
-				// Stop traversal on cyclical reference
-				if (context.visited_nodes.contains(ast_node.cell))
-					return false;
-
-				try visit(context.sheet, ast_node.cell, context.nodes, context.visited_nodes);
+				visit(context.sheet, ast_node.cell, context.nodes, context.sorted_nodes);
 			}
 			return true;
 		}
@@ -238,13 +257,13 @@ fn visit(
 
 	_ = try cell.ast.traverse(.middle, Context{
 		.sheet = sheet,
-		.node = node,
+		.cell = &cell,
 		.nodes = nodes,
-		.visited_nodes = visited_nodes,
+		.sorted_nodes = sorted_nodes,
 	});
 
-	visited_nodes.putAssumeCapacity(node, .permanent);
-	try nodes.insert(0, node);
+	nodes.putAssumeCapacity(node, .permanent);
+	sorted_nodes.appendAssumeCapacity(node);
 }
 
 pub fn getFilePath(sheet: Sheet) []const u8 {
@@ -391,6 +410,9 @@ pub const Position = struct {
 };
 
 pub const Cell = struct {
+	// TODO: reduce memory usage
+	//         store optional bit somewhere where it won't waste 63 bits
+	//         reduce Ast.NodeList to from 24 to 16 bytes
 	num: ?f64 = null,
 	ast: Ast = .{},
 
@@ -399,57 +421,55 @@ pub const Cell = struct {
 		cell.* = undefined;
 	}
 
-	pub fn isEmpty(cell: Cell) bool {
+	pub inline fn isEmpty(cell: Cell) bool {
 		return cell.ast.nodes.len == 0;
 	}
 
-	pub fn getValue(cell: *Cell, sheet: *Sheet) f64 {
+	pub inline fn getValue(cell: *Cell, sheet: *Sheet) f64 {
 		return cell.num orelse cell.eval(sheet);
 	}
 
-	pub fn eval(cell: *Cell, sheet: *Sheet) f64 {
+	pub fn eval(cell: *Cell, sheet: *Sheet) Allocator.Error!f64 {
 		const Context = struct {
 			sheet: *const Sheet,
-			stack: std.BoundedArray(Position, 512) = .{},
-			err: bool = false,
+			visited_cells: Map,
+
+			const Map = std.HashMap(Position, void, PositionContext, 80);
+			const EvalError = error{ CyclicalReference } || Ast.EvalError || Allocator.Error;
 	
-			pub fn evalCell(context: *@This(), pos: Position) ?f64 {
+			pub fn evalCell(context: *@This(), pos: Position) EvalError!?f64 {
 				// Check for cyclical references
-				for (context.stack.slice()) |p| {
-					if (p.hash() == pos.hash()) {
-						context.err = true;
-						return null;
-					}
+				const res = try context.visited_cells.getOrPut(pos);
+				if (res.found_existing) {
+					log.debug("CYCLICAL REFERENCE at {}", .{ pos });
+					return error.CyclicalReference;
 				}
 	
 				const _cell = context.sheet.getCell(pos) orelse return null;
+				if (_cell.num) |num| return num;
 	
-				if (context.stack.len == context.stack.capacity()) {
-					_ = context.stack.orderedRemove(0);
-				}
-	
-				context.stack.append(pos) catch unreachable;
-				const ret = _cell.ast.eval(context) catch |err| switch (err) {
-					error.NotEvaluable => {
-						context.err = true;
-						return null;
-					},
-				};
-				_ = context.stack.pop();
+				const ret = try _cell.ast.eval(context);
+				_ = context.visited_cells.remove(pos);
 				return ret;
 			}
 		};
 	
-		var context = Context{ .sheet = sheet };
-		const res = cell.ast.eval(&context);
+		// Only heavily nested cell references will heap allocate
+		var sfa = std.heap.stackFallback(2048, sheet.allocator);
+		var context = Context{
+			.sheet = sheet,
+			.visited_cells = Context.Map.init(sfa.get()),
+		};
+		const res = cell.ast.eval(&context) catch |err| switch (err) {
+			error.OutOfMemory => return error.OutOfMemory,
+			error.NotEvaluable, error.CyclicalReference => {
+				cell.num = null;
+				return 0;
+			},
+		};
 
-		if (context.err) {
-			cell.num = null;
-			return 0;
-		}
-
-		cell.num = res catch null;
-		return res catch 0;
+		cell.num = res;
+		return res;
 	}
 };
 

src/Parse.zig
@@ -245,12 +245,13 @@ pub const Slice = struct {
 		};
 	}
 
-	pub fn evalNode(ast: Slice, index: u32, context: anytype) EvalError!f64 {
+
+	pub fn evalNode(ast: Slice, index: u32, context: anytype) !f64 {
 		const node = ast.nodes.get(index);
 
 		return switch (node) {
 			.number => |n| n,
-			.cell => |pos| context.evalCell(pos) orelse 0,
+			.cell => |pos| try context.evalCell(pos) orelse 0,
 			.add => |op| (try ast.evalNode(op.lhs, context) + try ast.evalNode(op.rhs, context)),
 			.sub => |op| (try ast.evalNode(op.lhs, context) - try ast.evalNode(op.rhs, context)),
 			.mul => |op| (try ast.evalNode(op.lhs, context) * try ast.evalNode(op.rhs, context)),
@@ -261,7 +262,7 @@ pub const Slice = struct {
 		};
 	}
 
-	fn evalBuiltin(ast: Slice, builtin: Builtin, context: anytype) EvalError!f64 {
+	fn evalBuiltin(ast: Slice, builtin: Builtin, context: anytype) !f64 {
 		const args = ast.nodes.items(.data)[builtin.args].arg_list;
 		return switch (builtin.tag) {
 			.sum  => try ast.evalSum(args, context),
@@ -272,19 +273,19 @@ pub const Slice = struct {
 		};
 	}
 
-	pub fn evalSum(ast: Slice, args: ArgList, context: anytype) EvalError!f64 {
+	pub fn evalSum(ast: Slice, args: ArgList, context: anytype) !f64 {
 		var iter = ast.argIterator(args);
 
 		var total: f64 = 0;
 		while (iter.next()) |i| {
 			const tag = ast.nodes.items(.tags)[i];
-			total += if (tag == .range) ast.sumRange(ast.nodes.items(.data)[i].range, context)
+			total += if (tag == .range) try ast.sumRange(ast.nodes.items(.data)[i].range, context)
 					else try ast.evalNode(i, context);
 		}
 		return total;
 	}
 
-	pub fn sumRange(ast: Slice, range: BinaryOperator, context: anytype) f64 {
+	pub fn sumRange(ast: Slice, range: BinaryOperator, context: anytype) !f64 {
 		const pos1 = ast.nodes.items(.data)[range.lhs].cell;
 		const pos2 = ast.nodes.items(.data)[range.rhs].cell;
 
@@ -294,25 +295,25 @@ pub const Slice = struct {
 		var total: f64 = 0;
 		for (start.y..end.y + @as(u32, 1)) |y| {
 			for (start.x..end.x + @as(u32, 1)) |x| {
-				total += context.evalCell(.{ .x = @intCast(u16, x), .y = @intCast(u16, y) }) orelse 0;
+				total += try context.evalCell(.{ .x = @intCast(u16, x), .y = @intCast(u16, y) }) orelse 0;
 			}
 		}
 		return total;
 	}
 
-	pub fn evalProd(ast: Slice, args: ArgList, context: anytype) EvalError!f64 {
+	pub fn evalProd(ast: Slice, args: ArgList, context: anytype) !f64 {
 		var iter = ast.argIterator(args);
 		var total: f64 = 1;
 
 		while (iter.next()) |i| {
 			const tag = ast.nodes.items(.tags)[i];
-			total *= if (tag == .range) ast.prodRange(ast.nodes.items(.data)[i].range, context)
+			total *= if (tag == .range) try ast.prodRange(ast.nodes.items(.data)[i].range, context)
 					else try ast.evalNode(i, context);
 		}
 		return total;
 	}
 
-	pub fn prodRange(ast: Slice, range: BinaryOperator, context: anytype) f64 {
+	pub fn prodRange(ast: Slice, range: BinaryOperator, context: anytype) !f64 {
 		const pos1 = ast.nodes.items(.data)[range.lhs].cell;
 		const pos2 = ast.nodes.items(.data)[range.rhs].cell;
 
@@ -322,13 +323,13 @@ pub const Slice = struct {
 		var total: f64 = 1;
 		for (start.y..end.y + @as(u32, 1)) |y| {
 			for (start.x..end.x + @as(u32, 1)) |x| {
-				total *= context.evalCell(.{ .x = @intCast(u16, x), .y = @intCast(u16, y) }) orelse continue;
+				total *= try context.evalCell(.{ .x = @intCast(u16, x), .y = @intCast(u16, y) }) orelse continue;
 			}
 		}
 		return total;
 	}
 
-	pub fn evalAvg(ast: Slice, args: ArgList, context: anytype) EvalError!f64 {
+	pub fn evalAvg(ast: Slice, args: ArgList, context: anytype) !f64 {
 		var iter = ast.argIterator(args);
 		var total: f64 = 0;
 		var total_items: u32 = 0;
@@ -337,7 +338,7 @@ pub const Slice = struct {
 			const tag = ast.nodes.items(.tags)[i];
 			if (tag == .range) {
 				const range = ast.nodes.items(.data)[i].range;
-				total += ast.sumRange(range, context);
+				total += try ast.sumRange(range, context);
 
 				const p1 = ast.nodes.items(.data)[range.lhs].cell;
 				const p2 = ast.nodes.items(.data)[range.rhs].cell;
@@ -351,13 +352,13 @@ pub const Slice = struct {
 		return total / @intToFloat(f64, total_items);
 	}
 
-	pub fn evalMax(ast: Slice, args: ArgList, context: anytype) EvalError!f64 {
+	pub fn evalMax(ast: Slice, args: ArgList, context: anytype) !f64 {
 		var iter = ast.argIterator(args);
 		var max: ?f64 = null;
 
 		while (iter.next()) |i| {
 			const tag = ast.nodes.items(.tags)[i];
-			const res = if (tag == .range) ast.maxRange(ast.nodes.items(.data)[i].range, context) orelse continue
+			const res = if (tag == .range) try ast.maxRange(ast.nodes.items(.data)[i].range, context) orelse continue
 					else try ast.evalNode(i, context);
 			if (max == null or res > max.?)
 				max = res;
@@ -366,7 +367,7 @@ pub const Slice = struct {
 		return max orelse 0;
 	}
 
-	pub fn maxRange(ast: Slice, range: BinaryOperator, context: anytype) ?f64 {
+	pub fn maxRange(ast: Slice, range: BinaryOperator, context: anytype) !?f64 {
 		const pos1 = ast.nodes.items(.data)[range.lhs].cell;
 		const pos2 = ast.nodes.items(.data)[range.rhs].cell;
 
@@ -376,7 +377,7 @@ pub const Slice = struct {
 		var max: ?f64 = null;
 		for (start.y..end.y + @as(u32, 1)) |y| {
 			for (start.x..end.x + @as(u32, 1)) |x| {
-				const res = context.evalCell(.{ .x = @intCast(u16, x), .y = @intCast(u16, y) }) orelse continue;
+				const res = try context.evalCell(.{ .x = @intCast(u16, x), .y = @intCast(u16, y) }) orelse continue;
 				if (max == null or res > max.?)
 					max = res;
 			}
@@ -384,13 +385,13 @@ pub const Slice = struct {
 		return max;
 	}
 
-	pub fn evalMin(ast: Slice, args: ArgList, context: anytype) EvalError!f64 {
+	pub fn evalMin(ast: Slice, args: ArgList, context: anytype) !f64 {
 		var iter = ast.argIterator(args);
 		var min: ?f64 = null;
 
 		while (iter.next()) |i| {
 			const tag = ast.nodes.items(.tags)[i];
-			const res = if (tag == .range) ast.minRange(ast.nodes.items(.data)[i].range, context) orelse continue
+			const res = if (tag == .range) try ast.minRange(ast.nodes.items(.data)[i].range, context) orelse continue
 					else try ast.evalNode(i, context);
 			if (min == null or res < min.?)
 				min = res;
@@ -399,7 +400,7 @@ pub const Slice = struct {
 		return min orelse 0;
 	}
 
-	pub fn minRange(ast: Slice, range: BinaryOperator, context: anytype) ?f64 {
+	pub fn minRange(ast: Slice, range: BinaryOperator, context: anytype) !?f64 {
 		const pos1 = ast.nodes.items(.data)[range.lhs].cell;
 		const pos2 = ast.nodes.items(.data)[range.rhs].cell;
 
@@ -409,7 +410,7 @@ pub const Slice = struct {
 		var min: ?f64 = null;
 		for (start.y..end.y + @as(u32, 1)) |y| {
 			for (start.x..end.x + @as(u32, 1)) |x| {
-				const res = context.evalCell(.{ .x = @intCast(u16, x), .y = @intCast(u16, y) }) orelse continue;
+				const res = try context.evalCell(.{ .x = @intCast(u16, x), .y = @intCast(u16, y) }) orelse continue;
 				if (min == null or res < min.?)
 					min = res;
 			}
@@ -493,7 +494,7 @@ pub fn eval(
 	/// This function should return the value of the cell at the given position. It may do this
 	/// by calling this function.
 	context: anytype,
-) EvalError!f64 {
+) !f64 {
 	const slice = ast.toSlice();
 	return slice.evalNode(slice.rootNodeIndex(), context);
 }
@@ -1010,7 +1011,7 @@ test "Tokenizer" {
 test "Parse and Eval Expression" {
 	const t = std.testing;
 	const Context = struct {
-		pub fn evalCell(_: @This(), _: Position) ?f64 {
+		pub fn evalCell(_: @This(), _: Position) !?f64 {
 			unreachable;
 		}
 	};
@@ -1063,9 +1064,9 @@ test "Functions on Ranges" {
 	const Test = struct {
 		sheet: *Sheet,
 		
-		fn evalCell(context: @This(), pos: Position) ?f64 {
+		fn evalCell(context: @This(), pos: Position) !?f64 {
 			const cell = context.sheet.getCellPtr(pos) orelse return null;
-			return cell.eval(context.sheet);
+			return try cell.eval(context.sheet);
 		}
 
 		fn testSheetExpr(expected: f64, expr: []const u8) !void {
@@ -1144,7 +1145,7 @@ test "Splice" {
 	const allocator = arena.allocator();
 
 	const Context = struct {
-		pub fn evalCell(_: @This(), _: Position) ?f64 {
+		pub fn evalCell(_: @This(), _: Position) !?f64 {
 			return null;
 		}
 	};

src/main.zig
@@ -9,6 +9,7 @@ const ZC = @import("ZC.zig");
 const Allocator = std.mem.Allocator;
 
 var zc: ZC = undefined;
+var logfile: if (builtin.mode == .Debug) std.fs.File else void = undefined;
 
 pub fn main() !void {
 	if (builtin.mode == .Debug) {
@@ -16,8 +17,7 @@ pub fn main() !void {
 	}
 	defer {
 		if (builtin.mode == .Debug) {
-			logfile.?.close();
-			logfile = null;
+			logfile.close();
 		}
 	}
 
@@ -55,8 +55,6 @@ pub fn panic(msg: []const u8, trace: ?*std.builtin.StackTrace, ret_addr: ?usize)
     std.builtin.default_panic(msg, trace, ret_addr);
 }
 
-var logfile: ?std.fs.File = null;
-
 pub const std_options = struct {
 	pub const log_level = if (builtin.mode == .Debug) .debug else .info;
 	pub const logFn = if (builtin.mode == .Debug) log else std.log.defaultLog;
@@ -68,15 +66,11 @@ pub fn log(
 	comptime format: []const u8,
 	args: anytype,
 ) void {
-	if (logfile) |file| {
-		const writer = file.writer();
-		writer.print("[{s}] {s}: ", .{ @tagName(scope), @tagName(level) }) catch {};
-		writer.print(format, args) catch {};
-		writer.writeByte('\n') catch {};
-		file.sync() catch {};
-	} else {
-		std.log.defaultLog(level, scope, format, args);
-	}
+	const writer = logfile.writer();
+	writer.print("[{s}] {s}: ", .{ @tagName(scope), @tagName(level) }) catch {};
+	writer.print(format, args) catch {};
+	writer.writeByte('\n') catch {};
+	logfile.sync() catch {};
 }
 
 // Reference all tests in other modules

commit 9cd457f06e2aa58c4fc39187fb6130371be8ff02

Fix potential memory leak introduced in 5ecf895

example.zc
@@ -146,3 +146,5 @@ D20 = D19+C20
 E20 = E19+D20
 F20 = F19+E20
 G20 = G19+F20
+
+A22 = @sum(a0:g20)

src/Sheet.zig
@@ -185,11 +185,6 @@ pub fn update(sheet: *Sheet) Allocator.Error!void {
 
 	try sheet.rebuildSortedNodeList();
 
-	// Mark all cells as needing to be re-evaluated
-	for (sheet.cells.values()) |*cell| {
-		cell.num = null;
-	}
-
 	for (sheet.sorted_nodes.items) |pos| {
 		const cell = sheet.getCellPtr(pos).?;
 		_ = try cell.eval(sheet);
@@ -455,12 +450,14 @@ pub const Cell = struct {
 		};
 	
 		// Only heavily nested cell references will heap allocate
-		var sfa = std.heap.stackFallback(2048, sheet.allocator);
+		var sfa = std.heap.stackFallback(4096, sheet.allocator);
 		var context = Context{
 			.sheet = sheet,
 			.visited_cells = Context.Map.init(sfa.get()),
 		};
-		const res = cell.ast.eval(&context) catch |err| switch (err) {
+		defer context.visited_cells.deinit();
+
+		cell.num = cell.ast.eval(&context) catch |err| switch (err) {
 			error.OutOfMemory => return error.OutOfMemory,
 			error.NotEvaluable, error.CyclicalReference => {
 				cell.num = null;
@@ -468,8 +465,7 @@ pub const Cell = struct {
 			},
 		};
 
-		cell.num = res;
-		return res;
+		return cell.num.?;
 	}
 };
 